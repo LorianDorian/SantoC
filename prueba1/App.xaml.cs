7580536c2ae3c030fdf56649458e70a896d197a1
ISIS ASTRID MEMIJE PERALTA <L22320964@acapulco.tecnm.mx>
2025-04-26 14:49:02 -0600

    Color de barra de navegacion y superior

--- prueba1/App.xaml.cs
+++ prueba1/App.xaml.cs
@@ -1,17 +1,79 @@
-﻿using prueba1.Views;
-namespace prueba1
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using MauiWindow = Microsoft.Maui.Controls.Window;
+using AndroidWindow = Android.Views.Window;
+
+#if ANDROID
+using Android.Views;
+using Android.OS;
+using Microsoft.Maui.Platform;
+#endif
+
+namespace prueba1;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    public App()
     {
-        public App()
+        InitializeComponent();
+        MainPage = new NavigationPage(new MainPage())
         {
-            InitializeComponent();
+            BarBackgroundColor = Color.FromArgb("#FFF5E1"), // El color que quieras para la "franja"
+            BarTextColor = Colors.Black         // El color del texto (por ejemplo, negro para texto claro)
+        };
+#if ANDROID
+        if (OperatingSystem.IsAndroidVersionAtLeast(21))
+        {
+            AndroidWindow? androidStatusBarWindow = Microsoft.Maui.ApplicationModel.Platform.CurrentActivity?.Window;
+            if (androidStatusBarWindow != null)
+            {
+                androidStatusBarWindow.SetStatusBarColor(Colors.Beige.ToPlatform());
+            }
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
+        if (OperatingSystem.IsAndroidVersionAtLeast(23))
         {
-            return new Window(new NavigationPage(new MainPage()));
+            AndroidWindow? androidStatusBarIconWindow = Microsoft.Maui.ApplicationModel.Platform.CurrentActivity?.Window;
+            if (androidStatusBarIconWindow != null)
+            {
+                // Para iconos claros en fondo oscuro
+                androidStatusBarIconWindow.DecorView.SystemUiVisibility &= ~(StatusBarVisibility)SystemUiFlags.LightStatusBar;
+            }
         }
+#endif
+    }
 
+    protected override MauiWindow CreateWindow(IActivationState activationState)
+    {
+        MauiWindow mauiWindow = base.CreateWindow(activationState);
+
+        mauiWindow.HandlerChanged += (sender, e) =>
+        {
+#if ANDROID
+            if (OperatingSystem.IsAndroidVersionAtLeast(21))
+            {
+                AndroidWindow? androidWindow = Microsoft.Maui.ApplicationModel.Platform.CurrentActivity?.Window;
+                if (androidWindow != null)
+                {
+                    androidWindow.SetStatusBarColor(Colors.Beige.ToPlatform());
+                    androidWindow.SetNavigationBarColor(Colors.Beige.ToPlatform());
+
+                    if (OperatingSystem.IsAndroidVersionAtLeast(23))
+                    {
+                        androidWindow.DecorView.SystemUiVisibility &= ~(StatusBarVisibility)SystemUiFlags.LightStatusBar;
+                    }
+
+                    var insetsController = androidWindow.InsetsController;
+                    if (insetsController != null)
+                    {
+                        insetsController.SetSystemBarsAppearance(0, (int)WindowInsetsControllerAppearance.LightNavigationBars);
+                    }
+
+                }
+            }
+#endif
+        };
+
+        return mauiWindow;
     }
 }
